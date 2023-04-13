1343bbf1fb9b6be338f3fce1567965a9bca56875
diankasmilian <dianka.kulakevich@gmail.com>
2023-04-13 21:58:00 +0300

    finish task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('input');
+const divBox = document.querySelector('#boxes');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+btnCreate.addEventListener('click', () => {
+  const amount = Number(inputEl.value);
+  createBoxes(amount);
+});
+
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+const divEl = document.createElement('div');
+divEl.style.width = `${size}px`;
+divEl.style.height = `${size}px`;
+divEl.style.backgroundColor = getRandomHexColor();
+fragment.append(divEl);
+size += 10;
+  };
+  return divBox.append(fragment);
+}
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  divBox.innerHTML = '';
+}
