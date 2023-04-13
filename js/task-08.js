f4b1c69a558477ac3510093eeca00100985ec7d7
diankasmilian <dianka.kulakevich@gmail.com>
2023-04-13 15:24:41 +0300

    finish task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm (event) {
+event.preventDefault();
+
+const formElements = event.currentTarget.elements;
+
+const email = formElements.email.value;
+const password = formElements.password.value;
+
+const formData = {
+email,
+password,
+}
+
+
+if (email === '' || password === '') {
+   alert('Всі поля повинні бути заповнені');
+} else {
+   console.log(formData);
+   formEl.reset();
+};
+
+}
