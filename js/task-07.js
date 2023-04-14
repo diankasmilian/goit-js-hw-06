94d7167853ea06051bad40d80def6b0d0820500b
diankasmilian <dianka.kulakevich@gmail.com>
2023-04-14 12:52:28 +0300

    edit task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,13 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
+inputEl.value = inputEl.getAttribute('min');
 
 inputEl.addEventListener('input', onInputRange);
 
+
+
 function onInputRange (event) {
+   
    const inputValue = event.currentTarget.value;
    textEl.style.fontSize = `${inputValue}px`;
 }
